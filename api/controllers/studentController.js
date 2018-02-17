54851e168ffaf371b1f8943dad88de41b5c305b2
lucasbesen <lucassilvabesen@gmail.com>
2018-02-17 15:09:03 -0200

    Error handling

--- api/controllers/studentController.js
+++ api/controllers/studentController.js
@@ -4,27 +4,52 @@ const mongoose = require('mongoose');
 let Student = mongoose.model('Student');
 
 exports.list_all_students = async (ctx, next) => {
-    var student = await Student.find({});
-    ctx.body = student;
+    try {
+        var student = await Student.find({});
+        ctx.body = student;
+    } catch (err) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+    }
 };
 
 exports.create_a_student = async (ctx, next) => {
-    var new_student = new Student(ctx.request.body);
-    await new_student.save();
-    ctx.body = new_student;
+    try {
+        var new_student = new Student(ctx.request.body);
+        await new_student.save();
+        ctx.body = new_student;   
+    } catch (err) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+    }
 };
 
 exports.read_a_student = async (ctx, next) => {
-    var student = await Student.findById(ctx.params.studentId);
-    ctx.body = student;
+    try {
+        var student = await Student.findById(ctx.params.studentId);
+        ctx.body = student;
+    } catch (error) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+    }
 };
 
 exports.update_a_student = async (ctx, next) => {
-    var student = await Student.findOneAndUpdate({_id: ctx.params.studentId}, ctx.request.body, {new: true});
-    ctx.body = student;
+    try {
+        var student = await Student.findOneAndUpdate({_id: ctx.params.studentId}, ctx.request.body, {new: true});
+        ctx.body = student;
+    } catch (error) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+    }
 };
 
 exports.delete_a_student = async (ctx, next) => {
-    var student = await Student.remove({_id: ctx.params.studentId});
-    ctx.body = {message: 'Student successfully deleted'};
+    try {
+        var student = await Student.remove({_id: ctx.params.studentId});
+        ctx.body = {message: 'Student successfully deleted'};   
+    } catch (error) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+    }
 };
